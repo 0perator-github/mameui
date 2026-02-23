fb1496201c9ef2df4e5fbe92e8a0ffd1f652e944
hap <happppp@users.noreply.github.com>
2026-02-23 16:48:58 +0100

    New clones marked not working ----------------------------- Prophet-600 (set 2) [Siftware]

--- src/mame/sequential/prophet600.cpp
+++ src/mame/sequential/prophet600.cpp
@@ -49,20 +49,17 @@
 ***************************************************************************/
 
 #include "emu.h"
-#include "machine/clock.h"
-#include "cpu/z80/z80.h"
+
 #include "bus/midi/midi.h"
+#include "cpu/z80/z80.h"
 #include "machine/6850acia.h"
+#include "machine/clock.h"
 #include "machine/pit8253.h"
 
 #include "prophet600.lh"
 
 
 namespace {
-
-#define MAINCPU_TAG "z80"
-#define PIT_TAG     "pit"
-#define UART_TAG    "uart"
 
 enum
 {
@@ -78,10 +75,10 @@ enum
 class prophet600_state : public driver_device
 {
 public:
-	prophet600_state(const machine_config &mconfig, device_type type, const char *tag)
-	: driver_device(mconfig, type, tag),
-		m_maincpu(*this, MAINCPU_TAG),
-		m_acia(*this, UART_TAG),
+	prophet600_state(const machine_config &mconfig, device_type type, const char *tag) :
+		driver_device(mconfig, type, tag),
+		m_maincpu(*this, "maincpu"),
+		m_acia(*this, "uart"),
 		m_digits(*this, "digit%u", 0U),
 		m_dac(0),
 		m_scanrow(0),
@@ -91,9 +88,10 @@ public:
 
 	void prophet600(machine_config &config);
 
-private:
+protected:
 	virtual void machine_start() override ATTR_COLD;
 
+private:
 	void pit_ch0_tick_w(int state);
 	void pit_ch2_tick_w(int state);
 	void acia_irq_w(int state);
@@ -208,7 +206,7 @@ void prophet600_state::mask_w(uint8_t data)
 		m_comparitor &= ~0x04;
 	}
 
-//  printf("8253 gate = %x\n", data & 0x02);
+	//printf("8253 gate = %x\n", data & 0x02);
 }
 
 void prophet600_state::cv_w(uint8_t data)
@@ -243,13 +241,13 @@ void prophet600_state::potmux_w(uint8_t data)
 
 uint8_t prophet600_state::comparitor_r()
 {
-//  m_comparitor ^= 0x04;
+	//m_comparitor ^= 0x04;
 	return m_comparitor;
 }
 
 void prophet600_state::cpu_map(address_map &map)
 {
-	map(0x0000, 0x1fff).rom().region(MAINCPU_TAG, 0);
+	map(0x0000, 0x1fff).rom().region("maincpu", 0);
 	map(0x2000, 0x27ff).ram();
 	map(0x3000, 0x37ff).ram();
 	map(0x4000, 0x4001).w(FUNC(prophet600_state::dac_w));
@@ -259,7 +257,7 @@ void prophet600_state::cpu_map(address_map &map)
 
 void prophet600_state::io_map(address_map &map)
 {
-	map(0x00, 0x07).mirror(0xff00).rw(PIT_TAG, FUNC(pit8253_device::read), FUNC(pit8253_device::write));
+	map(0x00, 0x07).mirror(0xff00).rw("pit", FUNC(pit8253_device::read), FUNC(pit8253_device::write));
 	map(0x08, 0x08).mirror(0xff00).w(FUNC(prophet600_state::scanrow_w));
 	map(0x09, 0x09).mirror(0xff00).rw(FUNC(prophet600_state::comparitor_r), FUNC(prophet600_state::led_w));
 	map(0x0a, 0x0a).mirror(0xff00).rw(FUNC(prophet600_state::scan_r), FUNC(prophet600_state::potmux_w));
@@ -282,7 +280,7 @@ void prophet600_state::prophet600(machine_config &config)
 
 	config.set_default_layout(layout_prophet600);
 
-	pit8253_device &pit8253(PIT8253(config, PIT_TAG, XTAL(8'000'000)/4));
+	pit8253_device &pit8253(PIT8253(config, "pit", XTAL(8'000'000)/4));
 	pit8253.set_clk<0>(XTAL(8'000'000)/4);
 	pit8253.set_clk<1>(XTAL(8'000'000)/4);
 	pit8253.set_clk<2>(XTAL(8'000'000)/4);
@@ -305,11 +303,17 @@ static INPUT_PORTS_START( prophet600 )
 INPUT_PORTS_END
 
 ROM_START( prpht600 )
-	ROM_REGION(0x2000, MAINCPU_TAG, 0)
-	ROM_LOAD( "p600.bin",     0x000000, 0x002000, CRC(78e3f048) SHA1(61548b6de3d9b5c0ae76f8e751ece0b57de17118) )
+	ROM_REGION(0x2000, "maincpu", 0)
+	ROM_LOAD( "p600.bin", 0x0000, 0x2000, CRC(78e3f048) SHA1(61548b6de3d9b5c0ae76f8e751ece0b57de17118) )
+ROM_END
+
+ROM_START( prpht600a )
+	ROM_REGION(0x2000, "maincpu", 0)
+	ROM_LOAD( "1982_sci_six-0-2-1.10", 0x0000, 0x2000, CRC(ea89c882) SHA1(a0a21b365ac69c10005daa0802d07618a8b6bd19) )
 ROM_END
 
 } // anonymous namespace
 
 
-CONS( 1983, prpht600, 0, 0, prophet600, prophet600, prophet600_state, empty_init, "Sequential Circuits", "Prophet-600", MACHINE_NOT_WORKING|MACHINE_NO_SOUND )
+CONS( 1983, prpht600,  0,        0, prophet600, prophet600, prophet600_state, empty_init, "Sequential Circuits", "Prophet-600 (set 1)", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
+CONS( 1983, prpht600a, prpht600, 0, prophet600, prophet600, prophet600_state, empty_init, "Sequential Circuits", "Prophet-600 (set 2)", MACHINE_NOT_WORKING | MACHINE_NO_SOUND )
